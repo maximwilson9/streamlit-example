37644097a583b8cf953fb24a62b093a13fc774f5
maximwilson9 <106271518+maximwilson9@users.noreply.github.com>
2023-01-27 13:36:37 -0800

    Update WP.py
    fix import plt

--- WP.py
+++ WP.py
@@ -6,7 +6,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from scipy.stats import beta
 import seaborn as sns
 
